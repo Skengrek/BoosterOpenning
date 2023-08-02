538019695006f32bf9bed65f98bef3c767e47820
skengrek <armand.masse69@gmail.com>
2023-08-02 17:10:37 +0200

    Added debugpy listen at the right place

--- backend/manage.py
+++ backend/manage.py
@@ -2,17 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.conf import settings
 
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")    
 
-    if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-        import debugpy
-        print("Debugpy start listening the 5678 port aaaa")
-        debugpy.listen(("0.0.0.0", 5678))
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import debugpy
+            debugpy.listen(("0.0.0.0", 5678))
+            print('Attached!')
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
