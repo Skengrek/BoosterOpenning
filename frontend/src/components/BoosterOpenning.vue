89ad9bc4b5f8a8e367b88d1c782d90afdf592ece
Armand Massé <armand.masse@annalect.com>
2024-03-25 14:41:45 +0100

    wip

--- frontend/src/components/BoosterOpenning.vue
+++ frontend/src/components/BoosterOpenning.vue
@@ -96,7 +96,6 @@ export default {
             let el = document.getElementById('debug-center-card-point');
             el.style.left = `${this.centerX}px`;
             el.style.top = `${this.centerY}px`;
-            this.target.style.setProperty("--HoloOpacity", 35);
         },
 
         mouseMove(e) {
@@ -126,7 +125,6 @@ export default {
             this.target.style.setProperty("--yAxis", 0);
             this.target.style.setProperty("--xGlare", -100);
             this.target.style.setProperty("--yGlare", -100);
-            //this.target.style.setProperty("--HoloOpacity", 0);
         }
     },
     created() {
