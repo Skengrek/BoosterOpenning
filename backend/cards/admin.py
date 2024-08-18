92217edde41e68f8e62f29052d5a8c567666ff7b
skengrek <armand.masse69@gmail.com>
2024-08-18 19:03:53 +0200

    First Admin page

--- backend/cards/admin.py
+++ backend/cards/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from cards.models import Card, Set, Booster
 
 
-admin.site.register(Card)
-admin.site.register(Set)
-admin.site.register(Booster)
+@admin.register(Card)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name", "rarity", "extension_id")
+
+
+@admin.register(Set)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name", "series", "extension_id")
+
+
+@admin.register(Booster)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("user", "get_set_id", "number")
+
+    def get_set_id(self, obj):
+        return obj.set.extension_id
+    
+    get_set_id.admin_order_field  = 'set'  #Allows column order sorting
+    get_set_id.short_description = 'Extension ID'  #Renames column head
