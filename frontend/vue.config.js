769d57c820a8a4b9ea45923bcbadac4318e71b2c
skengrek <armand.masse69@gmail.com>
2024-08-23 16:42:45 +0200

    Test Prod

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      'skengrek.fr',
+      '192.168.1.1',
+    ],
+  },
+};
