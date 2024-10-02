526ca978ceeb5942663b40be9fad6ae11dce7587
skengrek <armand.masse69@gmail.com>
2024-10-02 14:43:44 +0200

    to prod

--- backend/users/token_claim.py
+++ backend/users/token_claim.py
@@ -5,6 +5,5 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         # Check if user logged in today
-        print()
         token = super().get_token(user)
         return token
